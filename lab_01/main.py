d99a80142d5664dd70eb786a41318a215eaf3c6c
Dinh Viet Anh <jokerva1010123@gmail.com>
2022-02-11 13:19:40 +0300

    Up file

--- lab_01/main.py
+++ lab_01/main.py
@@ -1,3 +1,11 @@
 import math
 
-n = int(input())
+data = open("data.txt", "r")
+data_x, data_y, data_y1 = [], [], []
+while True:
+    line = list(data.read().split())
+    if line == []:
+        break
+    print(line)
+
+data.close()
