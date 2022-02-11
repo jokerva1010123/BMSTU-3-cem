a4486f7e2f897979023669699b4b03b0183245bb
Dinh Viet Anh <jokerva1010123@gmail.com>
2022-02-11 18:07:13 +0300

    Update file

--- lab_01/main.py
+++ lab_01/main.py
@@ -1,11 +1,17 @@
 import math
 
-data = open("data.txt", "r")
+data_file = open("data.txt", "r")
 data_x, data_y, data_y1 = [], [], []
-line = [(data.read().split())]
-if len(line) == 0:
+data = data_file.read().split()
+data_file.close()
+if len(data) == 0:
     print("Пустый файл")
-    data.close()
     exit(0)
-if len(line) % 3:
-    print("Не ")
+if len(data) % 3:
+    print("Некорректный ввод")
+    exit(0)
+total_point = len(data) // 3
+for i in range(total_point):
+    data_x.append(data[i * 3])
+    data_y.append(data[i * 3 + 1])
+    data_y1.append(data[i * 3 + 2])
