206b5c7532dfce9f60262ecc8f8695ac1f58aec7
Dinh Viet Anh <jokerva1010123@gmail.com>
2022-02-11 13:25:30 +0300

    Up file

--- lab_01/main.py
+++ lab_01/main.py
@@ -2,10 +2,10 @@ import math
 
 data = open("data.txt", "r")
 data_x, data_y, data_y1 = [], [], []
-while True:
-    line = list(data.read().split())
-    if line == []:
-        break
-    print(line)
-
-data.close()
+line = [(data.read().split())]
+if len(line) == 0:
+    print("Пустый файл")
+    data.close()
+    exit(0)
+if len(line) % 3:
+    print("Не ")
